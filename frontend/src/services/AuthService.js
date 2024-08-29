45eee7d3e7896626f9104af1c0fa1778df82cf48
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2024-08-29 10:37:32 -0300

    feat(auth): Implement user login functionality with error handling and navigation to dashboard on success

--- frontend/src/services/AuthService.js
+++ frontend/src/services/AuthService.js
@@ -6,3 +6,8 @@ export const registerUser = async (email, password) => {
     const response = await axios.post(`${API_URL}register`, { email, password });
     return response.data;
 };
+
+export const loginUser = async (email, password) => {
+    const response = await axios.post(`${API_URL}login`, {email, password})
+    return response.data;
+};
